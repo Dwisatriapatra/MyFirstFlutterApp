87658cc76040f577fce941719a7b0a00813723b4
Dwisatriapatra <dwisatriapatra2000@gmail.com>
2022-07-16 16:46:43 +0700

    TextWidget

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,23 @@ class MyFlutterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("My First Flutter App"),),
-        body: Center(child: Text("Hello Dunia"),),
+        appBar: AppBar(
+          title: Text("My First Flutter App"),
+        ),
+        body: Center(
+            child: Container(
+          color: Colors.lightBlue,
+          width: 150,
+          height: 200,
+          child: Text(
+            "Saya sedang melatih kemampuan saya dalam membuat aplikasi dengan menggunakan flutter",
+            //overflow: TextOverflow.ellipsis,
+            //softWrap: false,
+            textAlign: TextAlign.center,
+            //maxLines: 2,
+            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        )),
       ),
     );
   }
